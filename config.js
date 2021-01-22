de4ca3267a09010116fad073a8d55ee19d51de3d
Jason Chan <jasonchan.newyork@gmail.com>
2021-01-22 03:13:49 -0500

    updated index

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://jason-chan:oMzqKIMM4EgGgyfX@cluster0.ivq6h.mongodb.net/merng2?retryWrites=true&w=majority',
+    // MONGODB: 'mongodb+srv://jason-chan:8RsAyp1jtLGY3pSp@cluster0.ivq6h.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'top class secret key'
+}
