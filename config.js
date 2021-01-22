e99a6cb6a7a7e86f7a172f707da2dd73dce3e9f9
Jason Chan <jasonchan.newyork@gmail.com>
2021-01-22 03:38:51 -0500

    fixed config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://jason-chan:oMzqKIMM4EgGgyfX@cluster0.ivq6h.mongodb.net/merng2?retryWrites=true&w=majority',
-    // MONGODB: 'mongodb+srv://jason-chan:8RsAyp1jtLGY3pSp@cluster0.ivq6h.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'top class secret key'
-}
